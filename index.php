32abd725396dbee23373d95e8fc60edbcf0bdae0
saku-to-39 <sakutonakano@gmail.com>
2022-01-04 13:38:13 +0900

    add toppage

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo  "todo アプリです";
-
+  /**テーブル定義
+   * posts 
+   * id int primary
+   * title text 
+   * 
+   * user
+   * id int primary
+   * username text
+   * email text
+   * password text
+   * 
+   * 要件
+   * CRUD処理ができること
+   * 新規登録、ログイン、ログアウトができること
+   * 
+   * bootstrapでヘッダー、フッターを作ること
+   * 
+   * 
+   */
+  
+?>
---
+++ top.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+  <heade>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <ul class="navbar-nav text-center">
+      <li class="nav-item"><a href="" class="nav-link" active>todolist</a></li>
+      <li class="nav-item"><a href="" class="nav-link">新規登録</a></li>
+      <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>  
+    </ul>
+  </nav>
+</header>
+
+<footer class="text-center pt-3 border-top">
+        &copy; 2022 SAMPLE Inc.
+</footer>
+</body>
+</html>
