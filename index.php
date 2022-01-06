9c67cfe14a26974f9f9ce79006ea9beaf5f729e4
saku-to-39 <sakutonakano@gmail.com>
2022-01-06 16:08:03 +0900

    一覧画面の表示

--- index.php
+++ index.php
@@ -22,18 +22,43 @@
    * 1.クエリを流す
    * foreachで表示させる
    */
-  echo "おはよう、こんにちは、こんばんは";
  $dsn = 'mysql:dbname=todo;host=localhost:8889';
  $pdo = new PDO($dsn, "root","root");
- var_dump($pdo);
  $sql = "select * from posts";
  $stmt = $pdo->query($sql);
 
  $posts = $stmt->fetchAll();
 
- 
- echo('<pre>');
+ /*echo('<pre>');
  var_dump($posts);
- echo('</pre>');
+ echo('</pre>');*/
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <h2>一覧画面</h2>
+  <hr>
+  <table class="table">
+    <tr>
+      <th class="text-center">ID</th>
+      <th class="text-center">TITLE</th>
+    </tr>
+    <?php foreach($posts as $post) :?>
+    <tr>
+      <td class="text-center"><?php echo $post['id'];?></td>
+      <td class="text-center"><?php echo $post['title'];?></td>
+    </tr>
+    <?php endforeach?>
+  </table>
+</body>
+</html>
