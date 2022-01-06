b980caaa3e2d35a42cf85a1e746840681070be5d
saku-to-39 <sakutonakano@gmail.com>
2022-01-06 15:55:02 +0900

    一覧処理のロジック

--- index.php
+++ index.php
@@ -17,6 +17,23 @@
    * bootstrapでヘッダー、フッターを作ること
    * 
    * 
+   * 
+   * 1.PDOインスタンスを生成する
+   * 1.クエリを流す
+   * foreachで表示させる
    */
-  
+  echo "おはよう、こんにちは、こんばんは";
+ $dsn = 'mysql:dbname=todo;host=localhost:8889';
+ $pdo = new PDO($dsn, "root","root");
+ var_dump($pdo);
+ $sql = "select * from posts";
+ $stmt = $pdo->query($sql);
+
+ $posts = $stmt->fetchAll();
+
+ 
+ echo('<pre>');
+ var_dump($posts);
+ echo('</pre>');
+
 ?>
